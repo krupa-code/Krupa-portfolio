ee39da42fd407c3427193282dcbc7e425f1557f1
krupa-code <krupabelvai@gmail.com>
2025-12-18 15:34:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function sayHi() {
+  alert("Hello! Thanks for visiting my portfolio 😊");
+}
+// Page load animation
+window.addEventListener("load", () => {
+  document.querySelector(".hero").classList.add("show");
+});
+
+// Button click interaction
+function sayHi() {
+  alert("Hi 👋 Thanks for visiting my portfolio!");
+}
